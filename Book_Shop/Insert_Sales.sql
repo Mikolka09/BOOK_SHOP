ff9fea939910f1216438e4c42777c5c7599bb56a
Микола Матіос <mikolka09@gmail.com>
2021-01-04 22:41:46 +0200

    04.01.2021

---
+++ Book_Shop/Insert_Sales.sql
@@ -0,0 +1,30 @@
+﻿SET NOCOUNT ON
+
+DECLARE @I INT
+SET @I = 0
+
+WHILE @I < 10000
+BEGIN
+
+INSERT INTO [Sales] ( [Price], [Quantity], [SaleDate], [BookId], [ShopId] )
+
+VALUES 
+(
+	 0,
+	(ABS(CHECKSUM(NEWID()) % 10) + 1),
+	(DATEADD(DAY, (ABS(CHECKSUM(NEWID())) % 365), '2020-01-01')
+	+ DATEADD(MINUTE, (ABS(CHECKSUM(NEWID())) % 600 + 480), 0)),
+	(ABS(CHECKSUM(NEWID()) % 68) + 1),
+	(ABS(CHECKSUM(NEWID()) % 14) + 1)
+)
+	SET @I = @I + 1
+END
+
+UPDATE 
+	Sales
+SET [Price] = B.[Price]
+FROM 
+	Sales S
+	JOIN Books B ON B.[Id] = S.[BookId] 
+WHERE
+	S.[BookId] = B.[Id]
---
+++ Book_Shop/Insert_Shops.sql
@@ -0,0 +1,15 @@
+﻿INSERT INTO [Shops] (  [Name],              [CountryId]  )
+			VALUES  (   N'Big Book Shop',        1       ),
+			        (   N'Books',                1       ),
+					(   N'Книжный Рай',          2       ),
+					(   N'Энциклопедия книг',    3       ),
+					(   N'Книжный',              2       ),
+					(   N'Libreria',             7       ),
+					(   N'Grande libreria',      7       ),
+					(   N'Gran librería',        10      ),
+					(   N'Libro paraíso',        10      ),
+					(   N'Книжный Домик',        3       ),
+					(   N'Book Boom',            1       ),
+					(   N'Buchboom',             5       ),
+					(   N'Entrepôt de livres',   6       ),
+					(   N'Paradis du livre',     6       )
